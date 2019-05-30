8238abe39a1596fb9820cf2c0505c6e82a121eaf
Maksim Ivanov <max24.for@gmail.com>
2019-05-30 21:25:14 +0300

    Initial Commit

---
+++ Tinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tinder
+//
+//  Created by Maksim Ivanov on 30/05/2019.
+//  Copyright © 2019 Maksim Ivanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
